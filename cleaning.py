8b34e2736b089d88d2226049478986bcebd00506
Tomas Mendizabal <100461170@alumnos.uc3m.es>
2024-10-21 16:42:54 +0200

    Reading of data

---
+++ cleaning.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+import re
+
+# paths usados
+pwd = os.getcwd()
+dataset_dir = f"{pwd}/DatasetsArqui"
+
+# leer datasets y guardarlos en un diccionario
+db = {} 
+for item in os.listdir(dataset_dir):
+    try:
+        file_name = re.findall("[A-Z][^A-Z]*", item)[0]
+        temp_db = pd.read_csv(f"{dataset_dir}/{item}")
+        db[file_name] = temp_db
+    except IndexError:
+        file_name = item.split('.')[0]
+        temp_db = pd.read_csv(f"{dataset_dir}/{item}")
+        db[file_name] = temp_db
+print(db.values())
+
+    
