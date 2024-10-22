5cbf44863f2d216f0fc45efd5f94a9f2c331602f
Tomas Mendizabal <100461170@alumnos.uc3m.es>
2024-10-22 17:16:14 +0200

    Load finished

--- src/load.py
+++ src/load.py
@@ -3,21 +3,24 @@ import os
 import re
 import numpy as np
 
-# paths usados
-pwd = os.getcwd()
-dataset_dir = f"{pwd}/DatasetsArqui"
 
-# leer datasets y guardarlos en un diccionario
-db = {} 
-for item in os.listdir(dataset_dir):
-    try:
-        file_name = re.findall("[A-Z][^A-Z]*", item)[0]
-        temp_db = pd.read_csv(f"{dataset_dir}/{item}")
-        db[file_name] = temp_db
-    except IndexError:
-        file_name = item.split('.')[0]
-        temp_db = pd.read_csv(f"{dataset_dir}/{item}")
-        db[file_name] = temp_db
+def load_db():
+    # paths usados
+    pwd = os.getcwd()
+    dataset_dir = f"{pwd}/DatasetsArqui"
+
+    # leer datasets y guardarlos en un diccionario
+    db = {} 
+    for item in os.listdir(dataset_dir):
+        try:
+            file_name = re.findall("[A-Z][^A-Z]*", item)[0]
+            temp_db = pd.read_csv(f"{dataset_dir}/{item}")
+            db[file_name] = temp_db
+        except IndexError:
+            file_name = item.split('.')[0]
+            temp_db = pd.read_csv(f"{dataset_dir}/{item}")
+            db[file_name] = temp_db
+    return db
 
 
 
